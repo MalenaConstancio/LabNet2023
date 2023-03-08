cb5208c1893fea390ac849c116a422aa80f25f86
Malena Constancio <maleconstancio@gmail.com>
2023-03-08 16:35:25 -0300

    Se agregan test unitarios a los metodos de la clase Logic

---
+++ LabNetPractica2Tests/MenuTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LabNetPractica2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LabNetPractica2.Tests
+{
+    [TestClass()]
+    public class MenuTests
+    {
+        [TestMethod()]
+        public void SeleccionarEjercicioTest()
+        {
+            //arrange
+            int opcionEsperada = 1;
+            //act
+            int opcionActual = Menu.SeleccionarEjercicio();
+            //assert
+            Assert.AreEqual(opcionEsperada, opcionActual);
+        }
+
+        //[TestMethod()]
+        //public void SolicitarUnNumeroTest()
+        //{
+        //    //arrange
+        //    //act
+        //    //assert
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void SolicitarDosNumerosTest()
+        //{
+        //    //arrange
+        //    //act
+        //    //assert
+        //    Assert.Fail();
+        //}
+    }
+}
---
+++ LabNetPractica2Tests1/LogicTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LabNetPractica2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNetPractica2.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DividirPorCeroTest()
+        {
+            //arrange
+            int divisor = 5;
+            //act
+            Logic.DividirPorCero(divisor);
+        }
+
+        [TestMethod()]
+        public void DividirTest()
+        {
+            //arrange
+            int dividendo=4;
+            int divisor=2;
+            decimal resultadoEsperado=2;
+            //act
+           decimal resultadoActual=Logic.Dividir(4,2);
+            //assert
+            Assert.AreEqual(resultadoEsperado, resultadoActual);
+        }
+
+
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DividirExceptionTest()
+        {
+            //arrange
+            int dividendo = 4;
+            int divisor = 0;
+            //act
+            Logic.Dividir(4, 0);
+        }
+    }
+}
