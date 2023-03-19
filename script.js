94bc6db295e16826ead8090444d2dd8c18c7abe8
Malena Constancio <maleconstancio@gmail.com>
2023-03-19 14:00:14 -0300

    Se sube lógica para sortear numero y mostrar pista

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
 
+//Lógica-->
 function SortearNumero(){
-    let numeroSorteado = Math.floor(Math.random()*50)+1;
-    return numeroSorteado;
+    let numSorteado = Math.floor(Math.random()*50)+1;
+    return numSorteado;
 }
 
-function VerificarNumeroIngresado(num){
 
+function GenerarPista(numIngresado){
+  
+    if(numIngresado==numSorteado){
+        mensaje="Adivinaste!" 
+    }
+    else if(numIngresado>numSorteado){
+        mensaje="Muy alto!"
+    } else{
+        mensaje="Muy bajo!"
+    }  
+    return mensaje;
+}
+
+function DescontarPuntos(score){
+    score-=5;
+    return score;
+}
+
+//Pantalla-->
+
+function IniciarJuego(){
+    numSorteado=SortearNumero();
+
+    sessionStorage.setItem("numSorteado",numSorteado);
+    sessionStorage.getItem("highScore",highScore); //cambiar por una cookie
+
+    document.querySelector("#numSorteado").textContent=numSorteado;
+    document.querySelector("#highScore").textContent=highScore;
+}
+
+function NuevoJuego(){
+    location.reload();
+}
+
+//Eventos-->
+document.querySelector("#btnIniciar").onclick = () => {
+    IniciarJuego();
+  }
+
+document.querySelector("#btnEnviar").onclick = () => {
+
+   let numIngresado = document.querySelector("#numIngresado").value;
+   let pista = GenerarPista(numIngresado);
+   document.querySelector("#numIngresado").value="";
+   document.querySelector("#mensaje").textContent=pista;
+}
+
+document.querySelector("#btnReiniciar").onclick=()=>{
+    NuevoJuego();
 }
