301822879512b7ccee48347e672968c24a37117f
Malena Constancio <maleconstancio@gmail.com>
2023-03-19 15:22:39 -0300

    Se sube lógica. Actualizar el score y guardar el highscore.

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function SortearNumero(){
 function GenerarPista(numIngresado){
   
     if(numIngresado==numSorteado){
-        mensaje="Adivinaste!" 
+        mensaje="Adivinaste!" ;
     }
     else if(numIngresado>numSorteado){
-        mensaje="Muy alto!"
+        mensaje="Muy alto!";
     } else{
-        mensaje="Muy bajo!"
+        mensaje="Muy bajo!";
     }  
     return mensaje;
 }
@@ -24,14 +24,18 @@ function DescontarPuntos(score){
     return score;
 }
 
+
 //Pantalla-->
 
 function IniciarJuego(){
     numSorteado=SortearNumero();
+    let score=100;
 
     sessionStorage.setItem("numSorteado",numSorteado);
-    sessionStorage.getItem("highScore",highScore); //cambiar por una cookie
 
+    let highScore= document.cookie;
+
+    document.querySelector("#score").textContent=score;
     document.querySelector("#numSorteado").textContent=numSorteado;
     document.querySelector("#highScore").textContent=highScore;
 }
@@ -39,7 +43,34 @@ function IniciarJuego(){
 function NuevoJuego(){
     location.reload();
 }
+function AsignarNuevoScore(pista){
 
+    score=Number(document.querySelector("#score").textContent);
+
+    if(pista!="Adivinaste!" ){
+         let scoreActualizado=DescontarPuntos(score);
+ 
+         if(scoreActualizado==0){
+             document.querySelector("#mensaje").textContent="Game Over!";
+             document.querySelector("#numIngresado").style.display="none";
+             document.querySelector("#btnEnviar").style.display="none";
+         }
+ 
+         document.querySelector("#score").textContent=scoreActualizado;
+    }else{
+         let nuevoScore = Number(document.querySelector("#score").textContent);
+         let highScore=document.cookie;
+ 
+         if(nuevoScore>highScore){
+             document.cookie=nuevoScore;
+             document.querySelector("#highScore").textContent=nuevoScore;
+         }
+ 
+         document.querySelector("#numIngresado").style.display="none";
+         document.querySelector("#btnEnviar").style.display="none";
+    }
+
+}
 //Eventos-->
 document.querySelector("#btnIniciar").onclick = () => {
     IniciarJuego();
@@ -49,8 +80,11 @@ document.querySelector("#btnEnviar").onclick = () => {
 
    let numIngresado = document.querySelector("#numIngresado").value;
    let pista = GenerarPista(numIngresado);
+   AsignarNuevoScore(pista);
    document.querySelector("#numIngresado").value="";
    document.querySelector("#mensaje").textContent=pista;
+   
+  
 }
 
 document.querySelector("#btnReiniciar").onclick=()=>{
