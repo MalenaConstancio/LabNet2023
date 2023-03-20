28ccff340e5f4971e6fdeb227efd18301b521bda
Malena Constancio <maleconstancio@gmail.com>
2023-03-20 13:24:36 -0300

    Se suben estilos actualizados

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function SortearNumero(){
 
 function GenerarPista(numIngresado){
     if(numIngresado==""){
-        mensaje="Ingresa un número" ;
+        mensaje="Por favor ingresa un número" ;
     }
     else if(numIngresado==numSorteado){
         mensaje="Adivinaste!" ;
@@ -32,12 +32,12 @@ function DescontarPuntos(score){
 function IniciarJuego(){
     document.querySelector("#juego").style.display="block";
     document.querySelector("#btnIniciar").style.display="none";
+   
     numSorteado=SortearNumero();
     let score=100;
 
     sessionStorage.setItem("numSorteado",numSorteado);
-
-    let highScore= document.cookie;
+    let highScore=document.cookie;
 
     document.querySelector("#score").textContent=score;
     document.querySelector("#numSorteado").textContent=numSorteado;
@@ -58,25 +58,31 @@ function AsignarNuevoScore(pista){
     if(pista!="Adivinaste!" ){
         let scoreActualizado=DescontarPuntos(score);
         if(scoreActualizado==0){
+            document.querySelector("#gameover").style.display="block";
             document.querySelector("#numSorteado").style.visibility="visible";
             document.querySelector("#numIngresado").style.display="none";
             document.querySelector("#btnEnviar").style.display="none";
-            document.querySelector("#mensaje").style.display="none";
+            document.querySelector("#instrucciones").style.visibility="hidden"
+            document.querySelector("#mensaje").style.visibility="hidden";
+            
         }
  
         document.querySelector("#score").textContent=scoreActualizado;
     }
     else{
-         let nuevoScore = Number(document.querySelector("#score").textContent);
-         let highScore=document.cookie;
+        let nuevoScore = Number(document.querySelector("#score").textContent);
+        let highScore=document.cookie;
  
-         if(nuevoScore>highScore){
-             document.cookie=nuevoScore;
-             document.querySelector("#highScore").textContent=nuevoScore;
-         }
-         document.querySelector("#numSorteado").style.visibility="visible";
-         document.querySelector("#numIngresado").style.display="none";
-         document.querySelector("#btnEnviar").style.display="none";
+        if(nuevoScore>highScore){
+            document.cookie=nuevoScore;
+            document.querySelector("#highScore").textContent=nuevoScore;
+        }
+     document.querySelector("#win").style.display="block";
+     document.querySelector("#numSorteado").style.visibility="visible";
+     document.querySelector("#numIngresado").style.display="none";
+     document.querySelector("#btnEnviar").style.display="none";
+     document.querySelector("#instrucciones").style.visibility="hidden"
+     document.querySelector("#mensaje").style.visibility="hidden";
     }
 
 }
@@ -90,6 +96,9 @@ function validaNumericos(event) {
 }
 
 document.querySelector("#btnIniciar").onclick = () => {
+    
+    document.getElementById("principal").style.backgroundColor ="#450e9d";
+    document.getElementById("btnReiniciar").style.visibility="visible";
     IniciarJuego();
   }
 
@@ -100,6 +109,7 @@ document.querySelector("#btnEnviar").onclick = () => {
    AsignarNuevoScore(pista);
    document.querySelector("#numIngresado").value="";
    document.querySelector("#mensaje").textContent=pista;
+   document.querySelector("#scores").style.visibility="visible";
    
   
 }
@@ -107,4 +117,5 @@ document.querySelector("#btnEnviar").onclick = () => {
 document.querySelector("#btnReiniciar").onclick=()=>{
     NuevoJuego();
 }
+
 
