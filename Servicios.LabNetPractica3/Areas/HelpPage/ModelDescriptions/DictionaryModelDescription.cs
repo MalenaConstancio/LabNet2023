ee13f07d2ea435d5fb62addbb36ce46a23aac267
MalenaConstancio <75854591+MalenaConstancio@users.noreply.github.com>
2023-03-28 16:56:45 -0300

    Delete Servicios.LabNetPractica3 directory
    Se borra Servicio duplicado y desactualizado.

--- Servicios.LabNetPractica3/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Servicios.LabNetPractica3.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Servicios.LabNetPractica3/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Servicios.LabNetPractica3.WebApiApplication" Language="C#" %>
