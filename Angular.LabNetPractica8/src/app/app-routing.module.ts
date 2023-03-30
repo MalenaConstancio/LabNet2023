e87c1f8c97935d6ad149f1e69d599953f5714d8f
Malena Constancio <maleconstancio@gmail.com>
2023-03-30 14:57:34 -0300

    Se suben rutas y se conectan componentes.

--- Angular.LabNetPractica8/src/app/app-routing.module.ts
+++ Angular.LabNetPractica8/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { HomeComponent} from './pages/home/home.component';
+import { SuppliersComponent } from './pages/suppliers/suppliers.component';
+ 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'home',component:HomeComponent},
+{path:'categories',component:CategoriesComponent},
+{path:'suppliers',component:SuppliersComponent},
+{path:'**',pathMatch: 'full',redirectTo:'/home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
